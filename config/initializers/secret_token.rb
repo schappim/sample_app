f32683187c242b98bf20dc6e4ba865e0592667ae
Marcus Schappi <admin@bigbird.local>
2011-05-06 18:53:24 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '45f45a68878b78015b178dba299c65536d09b20476653c893f7674b89f6d09eece41831e5e8dec24dec53485bbc4a10d04d1c661e341448f3ea365e2e31513a4'
